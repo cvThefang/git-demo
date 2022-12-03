25d3bb805ea0f2df7302b352fc7fdb37a4cacd8a
Thefang <Thefang@163.com>
2022-12-03 17:27:08 +0800

    first commit

---
+++ src/main/java/com/nuaa/GitTest.java
@@ -0,0 +1,12 @@
+package com.nuaa;
+
+/**
+ * @Author: Thefang
+ * @Create: 2022/12/3-17:18
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git !");
+    }
+}
