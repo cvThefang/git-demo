5e208d5aebea7e25ff8bc959714a44d86e01f54b
Thefang <Thefang@163.com>
2022-12-03 17:30:03 +0800

    second commit

--- src/main/java/com/nuaa/GitTest.java
+++ src/main/java/com/nuaa/GitTest.java
@@ -8,5 +8,7 @@ package com.nuaa;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git !");
+        System.out.println("Hello Git2 !");
+
     }
 }
